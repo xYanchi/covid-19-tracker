f28543e8e92f98ac2ed1f4855f11f34fc88f6a21
xYanchi <haymondyantech@gmail.com>
2020-10-10 16:55:27 -0400

    API call for list of countries

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    //The code inside will run once when the component loads
+    //async, send a request, wait, do retrieve only for name, value
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          //Now we got the whole json response
+          const countries = data.map((country) => (
+            {
+              name: country.country, // United Kingdom, United States, France
+              value: country.countryInfo.iso2 // UK, USA, FR
+            }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
   return (
     // BEM 
     <div className="app">
-      <h1>  Hello</h1>
+      <div className="app_header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+            {
+              countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </div>
+
+
+      {/* Header */}
+      {/* Title */}
+
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+
+      {/* Table */}
+      {/* Table */}
+
+      {/* Map */}
     </div>
   );
 }
