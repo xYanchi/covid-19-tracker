506f547a934442a3d9b1fc1469680b9703722fcf
xYanchi <haymondyantech@gmail.com>
2020-10-11 15:16:18 -0400

    Fix Capital words

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -132,7 +132,7 @@ function App() {
         <CardContent>
           <h3>Live Cases by Country</h3>
           <Table countries={tableData} />
-          <h3 className="app_graphTitle">Worldwide new {casesType}</h3>
+          <h3 className="app_graphTitle">Worldwide New {casesType}</h3>
           <LineGraph className="app_graph" casesType={casesType} />
         </CardContent>
       </Card>
